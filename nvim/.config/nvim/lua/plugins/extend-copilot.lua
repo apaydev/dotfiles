4e78893310d9448a5aeb33e2d67dc5cdf605edf1
Arturo Avila <mysticmetromx@gmail.com>
2024-07-01 00:22:53 -0600

    Modified snippets and autocompletion window behavior

---
+++ nvim/.config/nvim/lua/plugins/extend-copilot.lua
@@ -0,0 +1,17 @@
+return {
+  "zbirenbaum/copilot.lua",
+  opts = {
+    suggestion = {
+      enabled = true,
+      auto_trigger = true,
+      keymap = {
+        accept = "<M-l>",
+        accept_word = false,
+        accept_line = false,
+        next = "<M-]>",
+        prev = "<M-[>",
+        dismiss = "<C-]>",
+      },
+    },
+  },
+}
