7a04c2638267f9f5f0e2d25ab8fa9c5c5bb8392f
Arturo Avila <mysticmetromx@gmail.com>
2024-07-29 22:21:21 -0600

    Changed colorscheme
    I really like transparency, but it doesn't really work great for me WSL
    and in Alacritty. I may return and explore that in a few weeks, but I
    need a change in the colorscheme or I'm gonna go crazy.

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,29 +1,31 @@
 return {
-  {
-    "olimorris/onedarkpro.nvim",
-    priority = 1000, -- Ensure it loads first
-    config = function()
-      require("onedarkpro").setup({
-        styles = {
-          types = "italic, bold",
-          methods = "italic",
-          comments = "italic",
-          keywords = "bold",
-          functions = "italic",
-        },
-        colors = {
-          gray = "#6b6b6b",
-        },
-        options = {
-          transparency = true,
-        },
-      })
-    end,
-  },
+  -- {
+  --   "olimorris/onedarkpro.nvim",
+  --   priority = 1000, -- Ensure it loads first
+  --   config = function()
+  --     require("onedarkpro").setup({
+  --       styles = {
+  --         types = "italic, bold",
+  --         methods = "italic",
+  --         comments = "italic",
+  --         keywords = "bold",
+  --         functions = "italic",
+  --       },
+  --       colors = {
+  --         gray = "#6b6b6b",
+  --       },
+  --       options = {
+  --         transparency = true,
+  --       },
+  --     })
+  --   end,
+  -- },
+  -- {
+  { "folke/tokyonight.nvim", lazy = true, opts = { style = "moon" } },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "onedark_dark",
+      colorscheme = "tokyonight",
     },
   },
 }
