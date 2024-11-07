852ad2f789c87ca4e7aef45f162a720c6caaadcc
Arturo Avila <mysticmetromx@gmail.com>
2024-11-06 22:23:50 -0600

    Improved update checker, updated oh-my-posh config, and filtered out copilot.lua agent error

--- nvim/.config/nvim/lua/plugins/extend-noice.lua
+++ nvim/.config/nvim/lua/plugins/extend-noice.lua
@@ -4,5 +4,16 @@ return {
     presets = {
       lsp_doc_border = true, -- Adds a border to the hover docs and signature help.
     },
+    routes = {
+      {
+        filter = {
+          event = "msg_show",
+          any = {
+            { find = "Agent service not initialized" },
+          },
+        },
+        opts = { skip = true },
+      },
+    },
   },
 }
