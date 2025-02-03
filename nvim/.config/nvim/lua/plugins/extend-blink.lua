3a9b73ef75fea5ef02d1992dfeef9b4feaa4c30a
Arturo Avila <mysticmetromx@gmail.com>
2025-02-03 12:10:53 -0600

    feat: add blink config and change wallpaper

---
+++ nvim/.config/nvim/lua/plugins/extend-blink.lua
@@ -0,0 +1,19 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    completion = {
+      menu = {
+        border = "rounded",
+        winhighlight = "Normal:BlinkCmpSignatureHelp,FloatBorder:BlinkCmpSignatureHelpBorder",
+      },
+      documentation = {
+        window = {
+          border = "rounded",
+        },
+      },
+      ghost_text = {
+        enabled = false,
+      },
+    },
+  },
+}
